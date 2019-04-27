2d21fdfbe9fb44761f3db16b034b85be97a6b203
Milena Mariz <milenaferreira294@hotmail.com>
2019-04-27 11:26:06 -0300

    Add ClientMovementController
    Add client movement using nav agent and a display position.

---
+++ Assets/Scripts/ClientMovementController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class ClientMovementController : MonoBehaviour
+{
+    public NavMeshAgent clientAgent;
+
+    public GameObject product;
+
+    // Update is called once per frame
+    void Update()
+    {
+        clientAgent.SetDestination(product.transform.position);
+    }
+}
