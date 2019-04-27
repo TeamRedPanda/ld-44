19ad93cae7ccdd893dfd47cb85acf3b990316128
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2019-04-27 15:54:27 -0300

    Rename ItemDisplayController to ProductDisplayController

--- Assets/Scripts/Product/ProductDisplayController.cs
+++ Assets/Scripts/Product/ProductDisplayController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemDisplayController : MonoBehaviour
+public class ProductDisplayController : MonoBehaviour
 {
     public List<Product> ProductAssets;
 
