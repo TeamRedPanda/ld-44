ed7d604ff214e7720e73de9205a185eda8d3f3f5
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2019-04-29 20:11:33 -0300

    Add a tutorial screen

---
+++ Assets/Scripts/UI/ClickToStartButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickToStartButton : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("Starting game.");
+        SceneManager.LoadScene("Game");
+    }
+}
